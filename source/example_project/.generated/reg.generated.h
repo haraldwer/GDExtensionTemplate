b1c76ab1ad52f7df757e73f999382bb9d75effb9
Harald Wergeland <harwer009@gmail.com>
2023-09-30 16:28:19 +0200

    ignore fixes

--- source/example_project/.generated/reg.generated.h
+++ source/example_project/.generated/reg.generated.h
@@ -1,5 +1,5 @@
 #pragma once 
 
-#include "C:\Dev\GodotProject\source\example_project\extension.h"
-#include "C:\Dev\GodotProject\source\example_project\.generated\reg.generated.h"
-#include "C:\Dev\GodotProject\source\example_project\GDExample\gdexample.h"
+#include "C:\Dev\GodotProjectTestClone\source\example_project\extension.h"
+#include "C:\Dev\GodotProjectTestClone\source\example_project\.generated\reg.generated.h"
+#include "C:\Dev\GodotProjectTestClone\source\example_project\GDExample\gdexample.h"
