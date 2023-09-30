2037ad5e1ec563ce6cb701ae75c0e91294e0eb3e
Harald Wergeland <31646374+haraldwer@users.noreply.github.com>
2023-09-30 22:42:05 +0200

    Delete source/example_project/.generated directory

--- source/example_project/.generated/reg.generated.h
+++
@@ -1,5 +0,0 @@
-#pragma once 
-
-#include "C:\Dev\GodotProjectTestClone\source\example_project\extension.h"
-#include "C:\Dev\GodotProjectTestClone\source\example_project\.generated\reg.generated.h"
-#include "C:\Dev\GodotProjectTestClone\source\example_project\GDExample\gdexample.h"
