730374d62e9ea9873b835b2a97e51a6a00874e08
CHM <56677134+chocola-mint@users.noreply.github.com>
2023-11-08 20:33:32 +0800

    Add tests for include solver

---
+++ source/RegAutomation.Core.Tests/TestIncludeSolver.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegAutomation.Core.Tests
+{
+    [TestFixture]
+    internal class TestIncludeSolver
+    {
+        [Test]
+        public void TestSolve()
+        {
+            /*          C
+             * A -> B <   > E <- G
+             *          D   |   /
+             *              v /
+             *              F
+             * Cycle E-F-G is made up of headers with no types registered, so this should not throw.
+             */
+            var result = IncludeSolver.Solve(
+                new List<string>(){ "A", "B", "C", "D", "E", "F", "G" },
+                new List<List<string>>
+                {
+                    new List<string> { "B" }, // A
+                    new List<string> { "C", "D" }, // B
+                    new List<string> { "E" }, // C
+                    new List<string> { "E" }, // D
+                    new List<string> { "F" }, // E
+                    new List<string> { "G" }, // F
+                    new List<string> { "E" }, // G
+                },
+                new List<bool>{ true, false, true, true, false, false, false },
+                new List<string>{ "a", "b", "c", "d", "e", "f", "g" });
+            Assert.That(result, Is.EqualTo(
+                "#include \".generated/d.generated.h\"\n"
+                + "#include \".generated/c.generated.h\"\n"
+                + "#include \".generated/a.generated.h\"\n")
+                .Or.EqualTo(
+                "#include \".generated/c.generated.h\"\n"
+                + "#include \".generated/d.generated.h\"\n"
+                + "#include \".generated/a.generated.h\"\n"));
+            // Cycle E-F-G is now made up of headers with types registered, causing cyclic dependency, so this should throw.
+            Assert.Throws(typeof(Exception), () =>
+            {
+                IncludeSolver.Solve(
+                new List<string>(){ "A", "B", "C", "D", "E", "F", "G" },
+                new List<List<string>>
+                {
+                    new List<string> { "B" }, // A
+                    new List<string> { "C", "D" }, // B
+                    new List<string> { "E" }, // C
+                    new List<string> { "E" }, // D
+                    new List<string> { "F" }, // E
+                    new List<string> { "G" }, // F
+                    new List<string> { "E" }, // G
+                },
+                new List<bool>{ true, false, true, true, true, true, true },
+                new List<string>{ "a", "b", "c", "d", "e", "f", "g" });
+            });
+        }
+    }
+}
